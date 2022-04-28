2c52ecc97fbdf8244a979ad3c4c9f9b90488af7c
volha1 <71kiss@gmail.com>
2022-04-28 18:50:47 +0200

    feat: adding sticky header functionality

--- project-management-app/src/app/core/components/header/header.component.ts
+++ project-management-app/src/app/core/components/header/header.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  public isSticky: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll')
+  public onWindowScroll(): void {
+    this.isSticky = window.scrollY > 0 ? true : false;
   }
-
 }
