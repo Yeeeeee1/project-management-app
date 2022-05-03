493d980e7e3c3c0f0f58adeea068bcb3e892ed4c
volha1 <71kiss@gmail.com>
2022-05-04 01:59:06 +0200

    feat: add delete button

--- project-management-app/src/app/boards/boards.module.ts
+++ project-management-app/src/app/boards/boards.module.ts
@@ -1,17 +1,21 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { BoardsRoutingModule } from "./boards-routing.module";
-import { ColumnComponent } from "./components/column/column.component";
-import { BoardComponent } from "./pages/board/board.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { BoardsRoutingModule } from './boards-routing.module';
+import { ColumnComponent } from './components/column/column.component';
+import { BoardComponent } from './pages/board/board.component';
 
 @NgModule({
-    declarations: [BoardComponent, ColumnComponent],
-    imports: [
-      CommonModule,
-      RouterModule,
-      BoardsRoutingModule,
-    ],
-    exports: [],
-  })
-  export class BoardsModule {}
+  declarations: [BoardComponent, ColumnComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BoardsRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [],
+})
+export class BoardsModule {}
