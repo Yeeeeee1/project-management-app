df52549a11fc9105b4fba81d3ff42910ce93b916
Yeeeeee1 <KostyaEK2006@gmail.com>
2022-05-05 21:48:08 +0300

    feat: add proxy

--- project-management-app/src/app/boards/pages/boards/boards.component.ts
+++ project-management-app/src/app/boards/pages/boards/boards.component.ts
@@ -11,7 +11,12 @@ export class BoardsComponent implements OnInit {
 
   ngOnInit(): void {
     this.http
-      .get('http://localhost:4000/boards')
+      .get('http://localhost:4200/api/boards', {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwMWU0ZTNmYi1kZjg5LTQ5YTgtOWY2MS03MjFmNmFkNjhlYjgiLCJsb2dpbiI6InVzZXIwMDEiLCJpYXQiOjE2NTE3NzE5MzJ9.rwvQjDxX3xGh44glcfrenzXTJRLOLWnBUsZpua-uCy0',
+        },
+      })
       .subscribe((data) => console.log(data));
   }
 }
