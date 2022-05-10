6e7bcdedd0750e3f6ffdac4a4c82f51157d98bb8
Alina Husarava <alinagusarik@gmail.com>
2022-05-11 01:03:25 +0300

    feat:add styles for boards

--- project-management-app/src/app/boards/services/columns.service.ts
+++ project-management-app/src/app/boards/services/columns.service.ts
@@ -1,46 +1,48 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { map, Subject } from "rxjs";
-import { Column } from "../models/column";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Subject } from 'rxjs';
+import { Column } from '../models/column';
 
 @Injectable({
-    providedIn: 'root',
-  })
-  export class ColumnsService {
-    public columns$ = new Subject<Column[]>();
-    private lastOrderNumber:number;
-    private idBoard: string;
+  providedIn: 'root',
+})
+export class ColumnsService {
+  public columns$ = new Subject<Column[]>();
+  private lastOrderNumber: number;
+  private idBoard: string;
 
-    constructor(private router: Router, private http: HttpClient) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
-    public getColumns(
-      ): void {
-        this.http
-          .get(`boards/${this.idBoard}/columns`)
-          .pipe(map((response) => (response as Column[])))
-          .subscribe({
-            next: (columns) => {
-              this.columns$.next(columns);
-              this.lastOrderNumber = columns.slice(-1)[0].order;
-            },
-            error: () => this.router.navigate(['/error']),
-          });
-      }
-
-    public deleteColumn(columnId: string): void {
-      this.http.delete(`boards/${this.idBoard}/columns/${columnId}`).subscribe({
-        next: () => this.getColumns(),
+  public getColumns(): void {
+    this.http
+      .get(`boards/${this.idBoard}/columns`)
+      .pipe(map((response) => response as Column[]))
+      .subscribe({
+        next: (columns) => {
+          this.columns$.next(columns);
+          this.lastOrderNumber = columns.slice(-1)[0].order;
+        },
         error: () => this.router.navigate(['/error']),
-      })
-    }
+      });
+  }
 
-    public createColumn(title: string): void {
-      const column = { title: title, order: 1 + this.lastOrderNumber}
-      this.http.post(`boards/${this.idBoard}/columns`, column).subscribe({
-        next: () => this.getColumns(),
-        error: () => this.router.navigate(['/error']),
-    })
+  public deleteColumn(columnId: string): void {
+    this.http.delete(`boards/${this.idBoard}/columns/${columnId}`).subscribe({
+      next: () => this.getColumns(),
+      error: () => this.router.navigate(['/error']),
+    });
+  }
+
+  public createColumn(title: string): void {
+    const column = { title: title, order: 1 + this.lastOrderNumber };
+    this.http.post(`boards/${this.idBoard}/columns`, column).subscribe({
+      next: () => this.getColumns(),
+      error: () => {
+        console.log(Error);
+        this.router.navigate(['/error']);
+      },
+    });
   }
 
   public setIdBoard(id: string) {
