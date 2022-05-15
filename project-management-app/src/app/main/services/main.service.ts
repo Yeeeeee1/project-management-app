be7844944ec1869b32ba5d181c90dee682ced421
Yeeeeee1 <KostyaEK2006@gmail.com>
2022-05-15 16:29:48 +0300

    feat: adderror handlig

--- project-management-app/src/app/main/services/main.service.ts
+++ project-management-app/src/app/main/services/main.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { IMainBoardModel } from 'src/app/shared/models/IMainBoardModel';
 
 @Injectable({
@@ -9,36 +9,38 @@ import { IMainBoardModel } from 'src/app/shared/models/IMainBoardModel';
 export class MainService {
   clickCreateEvent: EventEmitter<any> = new EventEmitter();
   baseUrl: string = 'boards';
-  /* private headers = {
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhMDVjNmExNS1lMmE0LTQ5MTAtOTVlNi01MTM3ZDg1MWIzOWMiLCJsb2dpbiI6InVzZXIwMDFiaGIiLCJpYXQiOjE2NTI1NDk5MDZ9.G71eXFZVdLpjRw6xccnxOJl2BMjDQjodqRYl11z7yq0',
-    },
-  }; */
 
   constructor(private http: HttpClient) {}
+
+  handleError<T>(result?: T, operation = 'operation') {
+    return (error: any): Observable<T> => of(error.error.message);
+  }
 
   getBoards(): void {
     this.http
       .get(this.baseUrl)
+      .pipe(catchError(this.handleError<any>('getBoards', '')))
       .subscribe((data: any) => this.showResults(data));
   }
 
   createBoard(board: any): any {
     return this.http
       .post(this.baseUrl, board)
+      .pipe(catchError(this.handleError<any>('createBoard', '')))
       .subscribe((data: any) => this.getBoards());
   }
 
   changeName(board: any, id: any): any {
     return this.http
       .put(this.baseUrl + '/' + id, board)
+      .pipe(catchError(this.handleError<any>('chnageBoardName', '')))
       .subscribe((data: any) => this.getBoards());
   }
 
   deleteBoard(id: any): any {
     return this.http
       .delete(this.baseUrl + '/' + id)
+      .pipe(catchError(this.handleError<any>('deleteBoard', '')))
       .subscribe((data: any) => this.getBoards());
   }
 
