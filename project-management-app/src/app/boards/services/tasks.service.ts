6f41a0637b6cd2f0b84c05c730833b8afed8b370
volha1 <71kiss@gmail.com>
2022-05-18 00:43:48 +0200

    feat: add tasks service

---
+++ project-management-app/src/app/boards/services/tasks.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  constructor(private router: Router, private http: HttpClient) {}
+
+}
